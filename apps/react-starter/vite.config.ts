5b587f71b8eb1890183feca6869ac7c4fbcc49ab
Daniel Leroux <daniel.leroux@siemens.com>
2024-10-02 12:00:21 +0200

    fix base ref

--- apps/react-starter/vite.config.ts
+++ apps/react-starter/vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
-const url = process.env.REACT_URL ?? "/";
-const base = process.env.NODE_ENV === "production" ? url : "/";
+const base = process.env.REACT_BASE || "/";
 
 // https://vitejs.dev/config/
 export default defineConfig({
