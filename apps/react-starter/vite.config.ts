427a94bc69e3b435bae59652b2d9d0de16695527
Daniel Leroux <daniel.leroux@siemens.com>
2024-10-02 11:00:11 +0200

    add deployment

--- apps/react-starter/vite.config.ts
+++ apps/react-starter/vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
+const url = process.env.REACT_URL ?? "/";
+const base = process.env.NODE_ENV === "production" ? url : "/";
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base,
   plugins: [react()],
   resolve: {
     mainFields: ["module"],
