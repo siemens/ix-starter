4b5a8d4b6324f34bf6cf72ae6dbd8ad9511cd3fe
Lukas Zeiml <lukas.zeiml@siemens.com>
2025-04-14 11:37:32 +0200

    fix e2e

--- apps/react-starter/e2e/devices.e2e.ts
+++ apps/react-starter/e2e/devices.e2e.ts
@@ -1,25 +1,25 @@
 import { test, expect, Page } from "@playwright/test";
 
 async function filterDevicePageByDeviceName(page: Page, deviceName: string) {
-  const filter = page.getByLabel("Filter by");
-  const filterInput = filter.locator("input");
+  const categoryFilter = page.locator("ix-category-filter");
+  const filterInput = categoryFilter.locator("input");
 
   await filterInput.fill("Device name");
 
-  const dropdown = filter.locator("ix-dropdown");
+  const dropdown = categoryFilter.locator("ix-dropdown");
   await expect(dropdown.getByText("Categories")).toBeVisible();
 
   await page.keyboard.press("Tab");
   await page.keyboard.press("Enter");
 
-  await expect(filter.locator("ix-dropdown").getByText("Categories")).not.toBeVisible();
-  await expect(filter.locator("ix-dropdown").getByText("Device name")).toBeVisible();
+  await expect(categoryFilter.locator("ix-dropdown").getByText("Categories")).not.toBeVisible();
+  await expect(categoryFilter.locator("ix-dropdown").getByText("Device name")).toBeVisible();
 
   await filterInput.fill(deviceName);
   await page.keyboard.press("Enter");
   await page.mouse.click(0, 0);
 
-  await expect(filter.locator("ix-dropdown")).not.toBeVisible();
+  await expect(categoryFilter.locator("ix-dropdown")).not.toBeVisible();
 }
 
 test("filter for specific deviceName", async ({ page }) => {
