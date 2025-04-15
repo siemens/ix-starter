a484be1461fedd1c26abd8ea1fea8574bc18497b
lzeiml <lukas.zeiml@siemens.com>
2025-04-15 15:17:21 +0200

    ix-react-starter migrate to 3.0.0-alpha (#103)

--- apps/react-starter/src/pages/overview/components/incidents/overview/overview.test.tsx
+++ apps/react-starter/src/pages/overview/components/incidents/overview/overview.test.tsx
@@ -6,18 +6,21 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import { act } from "react";
-import { render, fireEvent, waitFor } from "@testing-library/react";
-import { expect, test } from "vitest";
+import { act, fireEvent, render, waitFor } from "@testing-library/react";
+import { beforeAll, expect, test } from "vitest";
 import IncidentsOverview from "./index";
+import { forceElementInternalsPolyfill } from "element-internals-polyfill";
+
+beforeAll(() => {
+  forceElementInternalsPolyfill();
+});
 
 test(`add filter input`, async () => {
   const screen = render(<IncidentsOverview></IncidentsOverview>);
-  const input = screen.getByLabelText("Filter devices") as HTMLInputElement;
+  const input = screen.getByPlaceholderText("Search") as HTMLInputElement;
 
   expect(input).toBeInTheDocument();
   await act(() => fireEvent.change(input, { target: { value: "robo1-net-sw17" } }));
-
   const items = screen.queryAllByTestId("incident-item");
   await waitFor(() => expect(items).toHaveLength(1));
 });
