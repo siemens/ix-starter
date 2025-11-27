78ae2e47e531d5bfa9c22d89c625a0bb9c043c85
Daniel Leroux <daniel.leroux@siemens.com>
2025-11-27 10:14:15 +0530

    fix copy theme script

---
+++ apps/angular-starter/scripts/copy-theme.ts
@@ -0,0 +1,13 @@
+import fs from 'fs-extra';
+import path from 'path';
+
+try {
+  const themePackage = import.meta.resolve('@siemens-ix/corporate-theme');
+  const theme = path.join(themePackage, '..', '..');
+  fs.copySync(theme, path.join(__dirname, '../src/assets/theme'), {
+    filter: (src) => !src.includes('corporate-theme-alternative/node_modules'),
+  });
+  console.log('Load additional theme');
+} catch (e) {
+  console.log('No additional theme found', e);
+}
