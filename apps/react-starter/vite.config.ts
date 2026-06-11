80ed758984853b6557e36e2c21abf4207fb4b41e
Daniel Leroux <daniel.leroux@siemens.com>
2026-06-11 13:50:48 +0200

    Revert "add oxe lint for react"
    This reverts commit a0ac3946a610775e4cd21c634e527f4befacea84.

--- apps/react-starter/vite.config.ts
+++ apps/react-starter/vite.config.ts
@@ -1,8 +1,17 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
   return {
+    base: env.BASE_PATH || '/',
     plugins: [react()],
+    build: {
+      sourcemap: true,
+    },
+    server: {
+      port: Number.parseInt(env.VITE_PORT || '3000'),
+      open: true,
+    },
   };
 });
--- apps/react-starter/vitest.config.ts
+++ apps/react-starter/vitest.config.ts
@@ -1,16 +1,16 @@
-import react from '@vitejs/plugin-react';
-import { playwright } from '@vitest/browser-playwright';
-import { configDefaults, defineConfig } from 'vitest/config';
+import { configDefaults, defineConfig } from "vitest/config";
+import { playwright } from "@vitest/browser-playwright";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   test: {
-    exclude: [...configDefaults.exclude, 'e2e/**'],
+    exclude: [...configDefaults.exclude, "e2e/**"],
     passWithNoTests: true,
     browser: {
       enabled: true,
       provider: playwright(),
-      instances: [{ browser: 'chromium' }],
+      instances: [{ browser: "chromium" }],
     },
   },
 });
